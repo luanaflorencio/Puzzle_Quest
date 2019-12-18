03490438ed61ce55c8ca1297aaca49c16e0e5eb9
luanaflorencio <lua.m.016@gmail.com>
2019-12-18 10:46:33 -0300

    movimentação dos blocos

--- blocos.js
+++ blocos.js
@@ -1,3 +1,7 @@
+const lines = 6;
+const columns = 8;
+
+
 class Bloco {
     constructor(forma, pontuacao) {
         this.forma = forma;
@@ -70,6 +74,75 @@ class Circulo extends Bloco {
         let cor = sorteandoCor();
         return `circulo-${cor}`;
     }
+}
+
+
+// function sorteandoObjeto() {
+//     let numeros = Math.floor(Math.random() * (+5 - +1)) + +1;
+//     console.log(numeros)
+//     switch (numeros) {
+//         case 1:
+//             return new Caveira("caveira", 2).iniciar();
+//             break;
+//         case 2:
+//             return new Estrela("estrela", 4).iniciar();
+//             break;
+//         case 3:
+//             return new Moeda("moeda", 3).iniciar();
+//             break;
+//         case 4:
+//             return new Circulo("circulo", 1, sorteandoCor()).iniciar();
+//             break;
+//     }
+// }
+
+function sorteandoCor() {
+    let cores = ["red", "green", "blue"];
+    let i = Math.floor(Math.random() * (+3 - +0)) + +0;
+    return cores[i];
+}
+
+
+//==================================================================
+function drag(ev) {
+    // ev.dataTransfer.setData("text", ev.target.class);
+    ev.dataTransfer.setData("bloco", ev.target.id);
+
+    document.getElementById(ev.target.id).classList.replace("bloco", "dragged")
+}
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    let id = document.getElementById(ev.dataTransfer.getData("bloco")).getAttribute("id");
+    let idElementoAtual = ev.target.id;
+
+    var movimentos = {
+        cima: (id - columns),
+        baixo: (Number(id) + columns),
+        direita: (Number(id) + 1),
+        esquerda: (id - 1)
+    }
+
+    if (idElementoAtual == movimentos.cima || idElementoAtual == movimentos.baixo || idElementoAtual == movimentos.direita || idElementoAtual == movimentos.esquerda) {
+        var data = ev.dataTransfer.getData("bloco");
+        let aux = document.getElementById(data);
+        let classAux = aux.getAttribute("class").split(" ")[1];
+
+        let elemtoAtual = ev.target.getAttribute("class").split(" ")[1];
+        ev.target.classList.replace(elemtoAtual, classAux);
+        aux.classList.replace(classAux, elemtoAtual);
+
+    }
+}
+
+//toggle classes of an element 
+function returnClasses(anyElement) {
+    anyElement.classList.replace("dragged", "bloco");
 }
 
 // function sorteandoObjeto() {
